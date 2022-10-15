3c512f4f0e1fbabb4e6f6f37156841c7e47f7fad
JoelNiemela <joel@niemela.se>
2022-10-14 21:41:47 -0500

    Add product_brand@show and @index routes

--- routes.php
+++ routes.php
@@ -7,4 +7,6 @@ return [
 	route('home') => 'page',
 	route('products') => 'product',
 	route('products', '(?<product_id>\d+)') => 'product@show',
+	route('product_brands') => 'product_brand',
+	route('product_brands', '(?<product_brand_id>\d+)') => 'product_brand@show',
 ];
