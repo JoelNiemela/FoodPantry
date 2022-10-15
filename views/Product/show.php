7f30472da59af9517f32b43dcd05b268453a9cd8
JoelNiemela <joel@niemela.se>
2022-10-14 22:40:59 -0500

    Display amount in stock and add restock button

--- views/Product/show.php
+++ views/Product/show.php
@@ -4,7 +4,14 @@
 </head>
 <body>
 <h1><?= $product->product_name ?></h1>
-<label>Max Storage: </label><?= $product->max_storage ?>
+<label>Max Storage: </label><?= $product->max_storage ?><br>
+<label>In Stock: </label><?= $product->in_stock ?><br>
+<h2>Stock items:</h2>
+<form action="<?= $product->product_id ?>/stock" method="POST">
+    <label>Amount: </label><input type="number" name="amount"><br>
+    <br>
+    <input type="submit">
+</form>
 <h2>Brands:</h2>
 <?php foreach ($brands as $brand): ?>
     <a href="../brands/<?= $brand->product_brand_id ?>">
