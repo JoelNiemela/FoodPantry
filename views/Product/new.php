ada33a26f8c3bb4da43c49ada978bbd4ae5c9fce
JoelNiemela <joel@niemela.se>
2022-10-14 22:04:49 -0500

    Create a form for the product@new view

--- views/Product/new.php
+++ views/Product/new.php
@@ -4,7 +4,16 @@
 </head>
 <body>
 <h1>Add a new product</h1>
+<form action="create" method="POST">
+    <label>Product name:</label><br>
+    <input type="text" name="product_name">
+    <br>
+    <label>Max Storage:</label><br>
+    <input type="number" name="max_storage">
+    <br>
+    <input type="submit">
+</form>
+<hr>
 <a href="../products">Back</a>
-<br>
 </body>
 </html>
