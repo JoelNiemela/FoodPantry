7a1cc7595c4b14cf6a5494af901e3127510a282b
JoelNiemela <joel@niemela.se>
2022-10-14 21:37:22 -0500

    Display brands in product@show view

--- views/Product/show.php
+++ views/Product/show.php
@@ -4,6 +4,13 @@
 </head>
 <body>
 <h1><?= $product->product_name ?></h1>
+<h2>Brands:</h2>
+<?php foreach ($brands as $brand): ?>
+    <a href="../brands/<?= $brand->product_brand_id ?>">
+        <?= $brand->brand_name ?>
+    </a>
+    <br>
+<?php endforeach ?>
 <a href="../products">Back to all products</a>
 <br>
 </body>
